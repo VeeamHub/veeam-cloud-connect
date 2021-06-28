9558b6c8039d848d7eee1c29ce7611f0b79b9b89
Chris Arceneaux <carcenea@gmail.com>
2021-06-28 21:35:13 +0000

    Initially improvements. Still not complete.
    - Migrating script from EM API to VSPC API
    - I'm currently able to auth to VSPC API and logout as well as create a Company.
    
    Next steps will be completing the remaining processes to onboard a new Cloud Connect tenant.
    
    Signed-off-by: Chris Arceneaux <carcenea@gmail.com>

--- veeam.class.php
+++ veeam.class.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * FILE
  */
@@ -9,9 +10,12 @@ require 'vendor/autoload.php';
 /**
  * Class Veeam
  */
-class Veeam {
+class Veeam
+{
   private $client;
-  private $session_id;
+  private $access_token;
+  private $cloud_connect_site;
+  private $cloud_connect_repository;
   private $backup_server_urn;
   private $backup_server_id;
   private $backup_repository_urn;
@@ -21,86 +25,119 @@ class Veeam {
   private $backup_resource;
   private $replication_create;
   private $replication_resource;
-
-  // Specify default values
-  private $backup_server          = "REPLACE_BACKUP_SERVER";
-  private $backup_repository      = "REPLACE_BACKUP_REPOSITORY";
-
-  private $hardware_plan          = "REPLACE_HARDWARE_PLAN";
-  private $lease_expiration       = "+3 months"; // see http://php.net/manual/en/function.strtotime.php
-
-  private $tenant_name            = "default-tenant-name"; // This should never happen. If so, you need to sanitize your input better.
-  private $tenant_description     = "Veeam RESTful API demo - default description";
-  private $tenant_resource_quota  = 102400;
-  private $tenant_password;       // Will be randomized in __construct();
+  private $company_id;
+  private $tenant_name;
+  private $tenant_username;
+  private $tenant_description;
+  private $tenant_password; // Will be randomized in __construct();
 
   /**
-   * @param $host
-   * @param $port
-   * @param $username
-   * @param $password
+   * @param str $base_url
+   * @param str $username
+   * @param str $password
+   * @param bool $backup
+   * @param bool $replication
    */
-  public function __construct($host, $port, $username, $password, $backup, $replication) {
-    $this->client = new GuzzleHttp\Client(array(
-      "base_url" => "http://" . $host . ":" . $port . "/api/",
-      "defaults" => array(
-        "auth" => array(
-          $username,
-          $password
-        ),
-        "headers" => array(
-          "Content-Type" => "text/xml"
-        ),
-      )
-    ));
+  public function __construct($base_url, $username, $password, $backup, $replication)
+  {
+    $config = include('config.php');
 
-    $response = $this->client->post('sessionMngr/?v=v1_2');
+    // Initializing API client
+    $this->client = new GuzzleHttp\Client([
+      'base_uri' => $base_url,
+      'timeout'  => $config['vspc_rest_timeout'],
+      'verify' => $config['vspc_tls_validation'],
+    ]);
 
+    try {
+      // Authenticating to VSPC API
+      $response = $this->client->request('POST', 'token', [
+        'form_params' => [
+          'grant_type' => 'password',
+          'username' => $username,
+          'password' => $password
+        ]
+      ]);
+
+      if ($response->getStatusCode() === 200) {
+        $result =  json_decode($response->getBody(), true);
+        $this->access_token = (string) $result['access_token'];
+
+        // Creating new client with access token specified
+        $this->client = new GuzzleHttp\Client([
+          'base_uri' => $base_url,
+          'timeout'  => $config['vspc_rest_timeout'],
+          'verify' => $config['vspc_tls_validation'],
+          'headers' => [
+            'Authorization' => 'Bearer ' . $result['access_token']
+          ]
+        ]);
+      } else {
+        throw new Exception("Unable to login to VSPC API. Please verify your connection information located in config.php is valid.");
+      }
+    } catch (GuzzleHttp\Exception\TransferException $e) {
+      throw new Exception("Unable to validate VSPC certificate as valid/trusted. Please make sure you are using a trusted certificate with matching connection information. If this is a lab/test environment, you can set 'vspc_tls_validation' in your config.php to FALSE.");
+    }
+
+    // Setting form selections
     $this->backup_create = $backup;
     $this->replication_create = $replication;
 
-    $this->session_id = (string) $response->getHeader('X-RestSvcSessionId');
-    $this->client->setDefaultOption('headers', array('X-RestSvcSessionId' => $this->session_id));
-
-    $this->tenant_password = $this->veeam_generate_password(12);
+    // Generating random password for new tenant account
+    $this->tenant_password = $this->veeam_generate_password($config['tenant_password_length']);
   }
 
-  public function __destruct() {
+  /**
+   *
+   */
+  public function __destruct()
+  {
     $this->veeam_delete_session();
   }
 
   /**
-  * @param int $length
-  *
-  * @return string
-  */
-  private function veeam_generate_password($length = 8) {
-    $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    return substr(str_shuffle($chars),0,$length);
-  }
-
-  /**
-   * @param bool $name
+   * @param int $length
    *
    * @return string
    */
-  private function veeam_get_backup_server($name = FALSE) {
-    $response = $this->client->get('backupServers');
+  private function veeam_generate_password($length = 12)
+  {
+    $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*';
+    return substr(str_shuffle($chars), 0, $length);
+  }
 
-    if ($name == FALSE) {
-      $backup_server_urn = (string) $response->xml()->Ref['UID'];
-      return $backup_server_urn;
-    }
-    else {
-      $name = strtolower($name);
-      foreach ($response->xml()->Ref as $backup_server) {
-        if (strtolower($backup_server['Name']) == $name) {
-          $backup_server_urn = (string) $backup_server['UID'];
-          return $backup_server_urn;
+  /**
+   * @param str $name
+   *
+   * @return string
+   */
+  private function veeam_get_cloud_connect_site($name = FALSE)
+  {
+    if ($name <> FALSE) {
+      // Retrieving Cloud Connect Site matching the name specified
+      $params = [
+        'query' => 'filter=[{
+          "property": "siteName",
+          "operation": "equals",
+          "value": "' . $name . '"
+        }]'
+      ];
+      $response = $this->client->get('infrastructure/sites', $params);
+
+      if ($response->getStatusCode() === 200) {
+        $body = json_decode($response->getBody(), true);
+
+        // In case no matches are returned
+        if ($body['meta']['pagingInfo']['total'] === 0) {
+          throw new Exception("No Cloud Connect Site found. Please verify 'vcc_server' located in config.php matches what's been defined in the VSPC web UI.");
         }
-      }
 
-      return "Not found!";
+        return $body['data'][0]['siteUid'];
+      } else {
+        throw new Exception("VSPC API call was unsuccessful with response code (" . $response->getStatusCode() . ")");
+      }
+    } else {
+      throw new Exception("'vcc_server' must be specified in config.php. Please define this value to avoid this error.");
     }
   }
 
@@ -110,19 +147,47 @@ class Veeam {
    *
    * @return string
    */
-  private function veeam_get_backup_repository($backup_server_id, $backup_repository_name) {
-    $response = $this->client->get('backupServers/' . $backup_server_id . '/repositories');
+  private function veeam_get_cloud_connect_repository($site_id, $name = FALSE)
+  {
+    if ($name <> FALSE) {
+      // Retrieving Cloud Connect Site matching the name specified
+      $params = [
+        'query' => 'filter=[{
+          "operation": "and",
+          "items": [
+            {
+              "property": "backupServerUid",
+              "operation": "equals",
+              "value": "' . $site_id . '"
+            },{
+              "property": "name",
+              "operation": "equals",
+              "value": "' . $name . '"
+            }
+          ]
+        }]'
+      ];
+      $response = $this->client->get('infrastructure/backupServers/repositories', $params);
 
-    foreach ($response->xml()->Ref as $backup_repository) {
-      if (strtolower($backup_repository_name) == strtolower($backup_repository['Name'])) {
-        return (string) $backup_repository['UID'];
+      if ($response->getStatusCode() === 200) {
+        $body = json_decode($response->getBody(), true);
+
+        // In case no matches are returned
+        if ($body['meta']['pagingInfo']['total'] === 0) {
+          throw new Exception("No Cloud Connect Repository found. Please verify 'vcc_repository' located in config.php matches what's shown in the VSPC web UI.");
+        }
+
+        return $body['data'][0]['instanceUid'];
+      } else {
+        throw new Exception("VSPC API call was unsuccessful with response code (" . $response->getStatusCode() . ")");
       }
+    } else {
+      throw new Exception("'vcc_repository' must be specified in config.php. Please define this value to avoid this error.");
     }
-
-    return "Not found!";
   }
 
-  private function veeam_get_hardware_plan($hardware_plan_name) {
+  private function veeam_get_hardware_plan($hardware_plan_name)
+  {
     $response = $this->client->get('cloud/hardwarePlans');
 
     foreach ($response->xml()->Ref as $hardware_plan) {
@@ -135,21 +200,12 @@ class Veeam {
   }
 
   /**
-   * @param $urn
-   *
-   * @return mixed
-   */
-  private function veeam_get_id_from_urn($urn) {
-    $parts = explode(':', $urn);
-    return array_pop($parts);
-  }
-
-  /**
    * @param $username
    *
    * @return bool
    */
-  private function veeam_check_username($username) {
+  private function veeam_check_username($username)
+  {
     $response = $this->client->get('cloud/tenants?format=Entity');
 
     foreach ($response->xml()->CloudTenant as $tenant) {
@@ -164,82 +220,76 @@ class Veeam {
   /**
    * @return bool
    */
-  private function veeam_delete_session() {
-    if (isset($this->session_id)) {
-      $response = $this->client->delete('logonSessions/' . base64_decode($this->session_id));
-      return $response->getStatusCode() == 204;
-    }
-    else {
+  private function veeam_delete_session()
+  {
+    if (isset($this->access_token)) {
+      // Retrieving current user
+      $response = $this->client->get('users/me');
+      $result =  json_decode($response->getBody(), true);
+      $user_uid = (string) $result['data']['instanceUid'];
+
+      // Revoking access token
+      $response = $this->client->delete('users/' . $user_uid . '/tokens');
+      return $response->getStatusCode() == 200;
+    } else {
       return FALSE;
     }
   }
 
   /**
-   * @param $task_id
-   * @param $link_type
-   */
-  private function veeam_task_subscriber($task_id, $link_type) {
-    $sleep = 1;
-
-    do {
-      $response = $this->client->get('tasks/' . $task_id);
-      $task_state = (string) $response->xml()->State;
-
-      if ($task_state == 'Finished') {
-
-        // Missing check to see if successful.
-        foreach ($response->xml()->Links->Link as $link) {
-          if ($link['Type'] == $link_type) {
-            return array_pop(explode("/", str_replace("?format=Entity", '', $link['Href'])));
-          }
-        }
-      }
-
-      sleep($sleep);
-      $sleep++;
-
-    } while ($task_state == "Running");
-  }
-
-  /**
-  Function from http://stackoverflow.com/a/5965940
-  **/
-
-  private function array_to_xml( $data, $xml_data ) {
-    foreach( $data as $key => $value ) {
-      if( is_array($value)) {
-        if( is_numeric($key)) {
-          $key = 'item'.$key; //dealing with <0/>..<n/> issues
-        }
-        $subnode = $xml_data->addChild($key);
-        $this->array_to_xml($value, $subnode);
-      } else {
-        $xml_data->addChild("$key",htmlspecialchars("$value"));
-      }
-    }
-
-    return $xml_data;
-  }
-
-  /**
-   * @param $root
-   * @param $type
-   * @param $href
-   * @param $content
+   * @param string $company_name
+   * @param string $alias
+   * @param string $tax_id
+   * @param string $email
+   * @param string $phone
+   * @param int $country
+   * @param int $state
+   * @param string $city
+   * @param string $street
+   * @param string $description
+   * @param string $zip_code
+   * @param string $website
+   * @param string $company_id
+   * @param string $subscription_plan
+   * @param bool $enable_alarms
    *
-   * @return mixed
+   * @return string
    */
-  private function create_xml($root, $type, $href, $content) {
-    global $client;
+  private function veeam_create_company($company_name, $alias = null, $tax_id = "", $email, $phone = "", $country = null, $state = null, $city = "", $street = "", $description, $zip_code = "", $website = "", $company_id = "", $subscription_plan = null, $enable_alarms = FALSE)
+  {
+    // Creating Company in VSPC
+    $params = [
+      'json' => [
+        "resellerUid" => null,
+        "organizationInput" => [
+          "name" => $company_name,
+          "alias" => null,
+          "taxId" => "",
+          "email" => $email,
+          "phone" => "",
+          "country" => null,
+          "state" => null,
+          "city" => "",
+          "street" => "",
+          "notes" => $description,
+          "zipCode" => "",
+          "website" => "",
+          "companyId" => ""
+        ],
+        "subscriptionPlanUid" => null,
+        "permissions" => [],
+        "IsAlarmDetectEnabled" => false
+      ]
+    ];
+    $response = $this->client->post('organizations/companies', $params);
 
-    $xml = new SimpleXmlElement('<' . $root . ' />');
-    $xml->addAttribute('xmlns', 'http://www.veeam.com/ent/v1.0');
-    $xml->addAttribute('xmlns:xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance');
-    $xml->addAttribute('Href', $this->client->getBaseUrl() . "{$href}");
+    if ($response->getStatusCode() === 200) {
+      $body = json_decode($response->getBody(), true);
 
-    $xml = $this->array_to_xml($content, $xml);
-
-    return $xml->asXml();
+      return $body['data']['instanceUid'];
+    } else {
+      throw new Exception("VSPC API call was unsuccessful with response code (" . $response->getStatusCode() . ")");
+    }
   }
 
   /**
@@ -249,7 +299,8 @@ class Veeam {
    * @param bool $enabled
    * @return string $tenant_result JSON encoded
    */
-  private function veeam_create_cloud_tenant($tenant_name = FALSE, $tenant_description = FALSE, $tenant_resource_quota = FALSE, $enabled = 0) {
+  private function veeam_create_tenant($tenant_name = FALSE, $tenant_description = FALSE, $tenant_resource_quota = FALSE, $enabled = 0)
+  {
     // Create tenant XML request
     // Refer to helpcenter.veeam.com for more information
     $url = 'cloud/tenants';
@@ -286,51 +337,30 @@ class Veeam {
     return json_encode($result);
   }
 
-   /**
-   *
+  /**
+   * @param string $username
+   * @param string $email
+   * @param string $full_name
+   * @param string $company_name
+   * @param int $resource_quota
+   * @param bool $enabled
+   * @return string $tenant_result JSON encoded
    */
-  public function run($tenant_name, $tenant_description = "", $tenant_resource_quota = FALSE, $enabled = 0) {
-    // Speed up the script by setting these to static values.
-    $this->backup_server_urn = $this->veeam_get_backup_server($this->backup_server);
-    $this->backup_server_id  = $this->veeam_get_id_from_urn($this->backup_server_urn);
+  public function run($username, $email, $full_name, $company_name)
+  {
+    $config = include('config.php');
 
-    // Override default values with input parameters if set
-    if (!empty($tenant_name)) {
-      $this->tenant_name = $tenant_name;
-    }
+    // Retrieving UIDs configured values
+    $this->cloud_connect_site = $this->veeam_get_cloud_connect_site($config['vcc_server']);
+    $this->cloud_connect_repository = $this->veeam_get_cloud_connect_repository($this->cloud_connect_site, $config['vcc_repository']);
 
-    if (!empty($tenant_description)) {
-      $this->tenant_description = $tenant_description;
-    }
+    // Setting description
+    $description = $full_name . " - " . $company_name;
 
-    if (!empty($tenant_resource_quota)) {
-      $this->tenant_resource_quota = $tenant_resource_quota;
-    }
+    // Creating Company in VSPC
+    $this->company = $this->veeam_create_company($company_name, null, "", $email, "", null, null, "", "", $description, "", "", "", null, FALSE);
 
-    if ($this->backup_create) {
-      $this->backup_repository_urn = $this->veeam_get_backup_repository($this->backup_server_id, $this->backup_repository);
-      $this->backup_repository_id = $this->veeam_get_id_from_urn($this->backup_repository_urn);
-
-      $this->backup_resource = array(
-        'BackupResource' =>  array(
-          'Name'            => 'cloud-' . $this->tenant_name . '-01',
-          'RepositoryUid'   => $this->backup_repository_urn,
-          'QuotaMb'         => $this->tenant_resource_quota
-        )
-      );
-    }
-
-    if ($this->replication_create) {
-      $this->hardware_plan_urn = $this->veeam_get_hardware_plan($this->hardware_plan);
-      $this->replication_resource = array(
-        'ComputeResource' =>  array(
-          'CloudHardwarePlanUid'          => $this->hardware_plan_urn,
-          'PlatformType'                  => 'VMware',
-          'UseNetworkFailoverResources'   => 'false'
-        )
-      );
-    }
-
-    echo $this->veeam_create_cloud_tenant($tenant_name, $tenant_description, $tenant_resource_quota, $enabled);
+    echo $this->company;
+    // echo $this->veeam_create_tenant($tenant_name, $tenant_description, $tenant_resource_quota, $enabled);
   }
 }
--- veeam.php
+++ veeam.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * FILE
  */
@@ -11,19 +12,33 @@ header('Content-Type: application/json');
 
 if (isset($_POST['action']) && (isset($_POST['create_backup']) || isset($_POST['create_replication'])) && $_POST['action'] == "create_tenant") {
 
+  $config = include('config.php');
 
-  $rest_server = 'REPLACE_SERVER';
-  $rest_port   = 'REPLACE_PORT';
-  $rest_user   = 'REPLACE_USER';
-  $rest_pass   = 'REPLACE_PASS';
-  
-  // Not very clean, but it works(tm)
+  $rest_url   = $config['vspc_rest_url'];
+  $rest_user  = $config['vspc_rest_user'];
+  $rest_pass  = $config['vspc_rest_pass'];
+
+  // Populating form values into variables
   $username = strtolower($_POST['username']);
-  $description = $_POST['full_name'] . " - " . $_POST['company_name'];
-  
-  $veeam = new Veeam($rest_server, $rest_port, $rest_user, $rest_pass, $_POST['create_backup'], $_POST['create_replication']);
+  $email = strtolower($_POST['email']);
+  $full_name = $_POST['full_name'];
+  $company_name = $_POST['company_name'];
 
-  // Create a user with 10 GB quota and leave them enabled.
-  // By default CC-R is disabled. Change FALSE to TRUE below to enable, and make sure to configure hardware plan in .class
-  $veeam->run($username, $description, 10240, FALSE);
+  // DEBUG: Populating empty form variables with default values
+  switch (TRUE) {
+    case (empty($username)):
+      $username = $config['default_username'];
+    case (empty($email)):
+      $email = $config['default_email'];
+    case (empty($full_name)):
+      $full_name = $config['default_full_name'];
+    case (empty($company_name)):
+      $company_name = $config['default_company_name'];
+  }
+
+  // Creating construct
+  $veeam = new Veeam($rest_url, $rest_user, $rest_pass, $_POST['create_backup'], $_POST['create_replication']);
+
+  // Create a user with the specified backup resource quota & leave the account enabled/disabled.
+  $veeam->run($username, $email, $full_name, $company_name);
 }
